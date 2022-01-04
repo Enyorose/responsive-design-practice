2efa8c8c0e3148f5fe63083158cd0e1ad8ac6c02
Jessica.W <jessica.watson1@live.com>
2022-01-04 13:21:59 -0700

    trying to fix flex box

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-//importing components into app
-import { Blog, Features, Footer, Possibility, WhatGPT3, Header } from './containers'
-import { CTA, Brand, Navbar } from './components'
-import './App.css'
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
 
+import './App.css';
 
-
-const app = () => {
-  return (
-    <div className='App'>
-      <div className='gradient__bg'>
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  )
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
-export default app
+export default App;
